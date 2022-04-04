6825327a40764fb5903e3cb07db57b8141c2dd2c
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-04-04 22:28:46 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -28,9 +28,23 @@ sudo snap install libreoffice
 sudo snap install foliate
 #Git
 sudo apt install git
-#Docker
-sudo snap install docker
+#Docker and compose
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker 
+docker-compose --version
 docker ps
