b4fbbff7e75424a81292a5e17094c0edc99088f2
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-04-28 21:20:12 +0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -3,15 +3,9 @@
 #     Update
 apt update && apt full-upgrade
 #     Nettools + Git
-apt-get install curl apt-transport-https git iptables-persistent net-tools
-
+apt-get install curl apt-transport-https git net-tools ca-certificates gnupg lsb-release
+tmux -L dialog-session new-session -d apt -y install iptables-persistent &&  tmux -L dialog-session send-keys Enter &&  tmux -L dialog-session send-keys
 #     Docker and compose
-apt-get update
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
@@ -22,12 +16,21 @@ apt-get install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-groupadd docker
-usermod -aG docker $USER
-newgrp docker 
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+systemctl restart docker
 #     Kuber
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
@@ -40,6 +43,18 @@ cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
+#modules
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+overlay
+EOF
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+overlay
+EOF
+sudo sh -c "echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> /etc/environment"
+modprobe br_netfilter
+modprobe overlay
 swapoff -a
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
@@ -48,6 +63,9 @@ kubectl version --client
 docker-compose --version
 docker ps
 
+#    groupadd docker
+#    sudo usermod -aG docker $USER
+#    newgrp docker 
 ###########################################################
 ##-----sudo nano /etc/hosts-----
 ## Add on all machines: 
@@ -58,36 +76,6 @@ docker ps
 ##-----sudo nano /etc/fstab----- 
 ## Comment line with swap 0 1
 ###########################################################
-##     Add modules
-##-----sudo nano /etc/modules-load.d/k8s.conf-----
-## Add lines:
-#      br_netfilter
-##     overlay
-## Start:
-##     modprobe br_netfilter
-##     modprobe overlay
-###########################################################
-##-----sudo nano /etc/docker/daemon.json----- 
-##{
-##  "exec-opts": ["native.cgroupdriver=systemd"],
-##  "log-driver": "json-file",,
-##  "log-opts": {
-##    "max-size": "100m"
-##  },
-##  "storage-driver": "overlay2",
-##  "storage-opts": [
-##    "overlay2.override_kernel_check=true"
-##  ]
-##}
-##
-## Start: systemctl restart docker
-###########################################################
-##-----sudo nano /etc/environment----- 
-## Add line: 
-##     export KUBECONFIG=/etc/kubernetes/admin.conf
-###########################################################
-
-
 
 ##     PORTS
 ##-----masternode-----
@@ -97,10 +85,6 @@ docker ps
 ##     iptables -I INPUT 1 -p tcp --match multiport --dports 10250,30000:32767 -j ACCEPT
 ##     netfilter-persistent save
 
-
-
 ##     https://www.dmosk.ru/instruktions.php?object=kubernetes-ubuntu
-
-
 
 ##     yes | sudo bash s.sh
