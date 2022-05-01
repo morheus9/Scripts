c2a6018ac516767fca95ef5f4e66725658656660
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-05-01 12:58:27 +0300

    Update d.sh

--- d.sh
+++ d.sh
@@ -14,10 +14,10 @@ make altinstall
 apt install python3-pip
 -H pip install -U pipenv
 #     Vscode
-sudo apt-get install wget gpg
+apt-get install wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 apt install apt-transport-https
 apt update
@@ -30,16 +30,16 @@ wget -O- https://dl.google.com/linux/linux_signing_key.pub |gpg --dearmor > /etc
 apt update
 apt install google-chrome-stable
 #     Vbox
-sudo apt update && sudo apt -y upgrade
-sudo apt -y install wget gnupg2
+apt update && sudo apt -y upgrade
+apt -y install wget gnupg2
 sudo apt install linux-headers-$(uname -r) dkms
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bullseye contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-sudo apt update
-sudo apt install virtualbox-6.1
+apt update
+apt install virtualbox-6.1
 wget https://download.virtualbox.org/virtualbox/6.1.26/Oracle_VM_VirtualBox_Extension_Pack-6.1.26.vbox-extpack
-sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.26.vbox-extpack     
+VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.26.vbox-extpack     
 
 python3.10 --version
 
